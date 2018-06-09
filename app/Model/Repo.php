f4e697283c0d6e180e6ef1028754206449ad04a9
Ignas Strimaitis <ignas.strimaitis@protonmail.com>
2018-06-09 13:58:28 +0300

    Add name and description fields to repo endpoint

--- app/Model/Repo.php
+++ app/Model/Repo.php
@@ -5,6 +5,8 @@ namespace App\Model;
 class Repo
 {
     public $id;
+    public $name;
+    public $description;
     public $open_issues;
 
     /**
@@ -13,6 +15,8 @@ class Repo
     public function __construct($data)
     {
         $this->id = $data['id'] ?? null;
+        $this->name = $data['name'] ?? null;
+        $this->description = $data['description'] ?? null;
         $this->open_issues = $data['open_issues'] ?? null;
     }
 }
