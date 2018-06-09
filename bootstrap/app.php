4a7d743bf4aaf58f9172adcd50730b6534f2cbd9
Ignas Strimaitis <ignas.strimaitis@protonmail.com>
2018-06-09 15:03:00 +0300

    Clean up! Remove unnused classes.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -72,8 +72,6 @@ $app->routeMiddleware([
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\HttpClientServiceProvider::class);
 
