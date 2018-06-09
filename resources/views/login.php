db0507a046336d0e6c372ad4ba42c7061cae6cf5
Ignas Strimaitis <ignas.strimaitis@protonmail.com>
2018-06-09 12:20:21 +0300

    Provide instructions for creating the Client id

--- resources/views/login.php
+++ resources/views/login.php
@@ -6,5 +6,9 @@
     <p>
       <a href="https://github.com/login/oauth/authorize?scope=user:email&client_id=<?php echo $clientId ?>&redirect_uri=<?php echo $redirectUri ?>">Login with Github</a></a>
     </p>
+    <p>
+      If that link doesn't work, you can provide your own Client ID and Client secret in .env settings file.<br>
+      <a href="https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/">https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/</a>
+    </p>
   </body>
 </html>
